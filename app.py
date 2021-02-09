84aa2d3cf440e2f156f02029cca56bdc50412f31
Kiran A K <34866636+krnak07@users.noreply.github.com>
2021-02-09 11:28:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from calculator import Calculator
+
+if __name__ == '__main__':
+
+    # calculator class object
+    calObj = Calculator()
+
+    # different operations of calculator
+    print(calObj.add(1, 2))
+    print(calObj.subtract(1, 2))
+    print(calObj.multiply(1, 2))
+    print(calObj.divide(1, 2))
